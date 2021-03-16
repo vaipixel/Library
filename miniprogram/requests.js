1ae072c504c9190d6b1aff14266d974cd130ddaa
T-Oner <1@fxcdev.com>
2021-03-16 18:43:36 +0800

    添加客户端网络请求封装

--- miniprogram/requests.js
+++ miniprogram/requests.js
@@ -8,3 +8,51 @@ async function baseRequest(api, data) {
     });
     return result.result;
 }
+
+async function login(userName, passwd) {
+    return baseRequest('login', {userName, passwd})
+}
+
+async function register(userName, passwd) {
+    return baseRequest('register', {userName, passwd})
+}
+
+async function getLibraries() {
+    return baseRequest('getLibraries')
+}
+
+async function appointSeat(appoint) {
+    return baseRequest('appointSeat', appoint)
+}
+
+async function checkIn(appointId) {
+    return baseRequest('checkIn', {appointId})
+}
+
+async function checkOut(appointId) {
+    return baseRequest('checkOut', {appointId})
+}
+
+async function getNoncomplianceRecord(userId) {
+    return baseRequest('getNoncomplianceRecord', {userId})
+}
+
+async function getLibraryAppointInfo(libraryId) {
+    return baseRequest('getLibraryAppointInfo', {libraryId})
+}
+
+async function getLibrarySeatBookedInfo(query) {
+    return baseRequest('getLibrarySeatBookedInfo', query)
+}
+
+module.exports = {
+    login: login,
+    register: register,
+    getLibraries: getLibraries,
+    appointSeat: appointSeat,
+    checkIn: checkIn,
+    checkOut: checkOut,
+    getNoncomplianceRecord: getNoncomplianceRecord,
+    getLibraryAppointInfo: getLibraryAppointInfo,
+    getLibrarySeatBookedInfo: getLibrarySeatBookedInfo
+}
